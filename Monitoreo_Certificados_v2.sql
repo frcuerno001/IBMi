b8e44243692343a39315a68b364dd255f9ca53eb
frcuerno001 <117700461+frcuerno001@users.noreply.github.com>
2023-10-05 18:13:46 -0600

    monitoreo de certificados digitales ibm i

---
+++ Monitoreo_Certificados_v2.sql
@@ -0,0 +1,23 @@
+﻿
+  
+  
+     
+   
+   
+  
+select CERTIFICATE_LABEL, (days(VALIDITY_START) - days(VALIDITY_END)) as fin_cert
+  from table (
+      qsys2.certificate_info(certificate_store_password => '*NOPWD')
+    )
+  
+  validity_start  
+    
+  where CERTIFICATE_LABEL not like('GeoTrust%')
+  and CERTIFICATE_LABEL not like('Equifax%')
+  and CERTIFICATE_LABEL not like('Microsoft%')
+  and CERTIFICATE_LABEL not like('Thawte%')
+  and CERTIFICATE_LABEL not like('Verisign%')
+  and CERTIFICATE_LABEL not like('LOCAL%')
+  and validity_start <= current_date + 1 month
+  order by validity_end asc;
+  
